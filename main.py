a1f0de071808926fd6199f14dc3028ace2a1ba9a
hosseinvejdani <vejdani.shojae@gmail.com>
2021-06-26 20:25:32 +0430

    1 - add info.json to gitignore 2 - add some code for reading telegram infos from json file 3 - modify code for dublicate msg restriction

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler
+import json
 import tracemalloc
 tracemalloc.start()
 
 def send_message(msg):
-    app.send_message(channel_name, msg)
+    app.send_message('SpinorApps', msg)
 
-
-api_id = "2285336"
-api_hash = "ce4084bb00243d6d9294e73550f5f6e4"
-phone = "+989907160406"
-username = "HosseinVejdani"
-channel_name = 'SpinorApps'
+# ===========================================================
+# info.json containes telegram account informations like this:
+# {
+#     "api_id" : "xxxxxxx",
+#     "api_hash" : "xxxxxxxxxxxxxxxxxxxxxxxx",
+#     "phone" : "+989xx716xxxx",
+#     "username" : "xxxx",
+#     "password" : "xxxxxxx"
+# }
+# you shoud make a info.json in the main directory with
+# your own telegram account infos
+info = json.load('info.json')
+api_id = info['api_id']
+api_hash = info['api_hash']
+phone = info['phone']
+username = info['username']
+password = info['password']
 
 app = Client(
     username,
     phone_number=phone,
-    password='hvsh128',
+    password=password,
     api_id=api_id,
     api_hash=api_hash
 )
@@ -27,57 +40,30 @@ channels['danstany_ajib'] = '@danstany_ajib'
 channels['Dansstaniha'] = '@Dansstaniha | دانستنی ها ™'
 channels['Ajayebe_jahann'] = '@Ajayebe_jahann'
 channels['mytestchch'] = '@mytestchch'
+channels['irWonders'] = '@irWonders'
 
-print()
-
-
-# @app.on_message(filters.channel('SpinorApps'))
-# async def hello(client, message):
-#     # await message.reply_text(f"Hello ...")
-#     print(message)
-#     print(type(message))
-#     # print(help(message))
-#     print(dir(message))
-#     print('msg id ', message.message_id)
-#     print('from user ', message.from_user)
-#     print('text ', message.text)
-#     print('views ', message.views)
-#     fid = "AgACAgQAAx0EUSHeKQACGgFg1WBkA86_JE143YnwJslmfMa_XwAClLMxG5BMiVIEG-RTh5JC4NLs-ixdAAMBAAMCAAN5AAMTIAMAAR4E"
-#     capt = "معجزات باورنکردنیه بغل کردن !\n\nبغل کردن معجزه میکند. یک آغوش گرم باعث ترشح دوپامین در مغز میشود که به هورمون لذت مشهور است. دقیقاً به همین خاطر است که فوراً احساس امنیت و آرامش میکنید. دانشمندان میگویند بغل کردن فشار خون و اضطراب را کاهش میدهد و روحیه را تقویت میکند\n\n@SpinorMag | اسپینور مگ"
-#     await app.send_cached_media('SpinorApps', file_id=fid, caption=capt)
-
-
-# app.run()
-
-
-
-
-
-from pyrogram.handlers import MessageHandler
-
+file_ids = []
 
 def my_function(client, message):
-    # print(message)
-    # print(type(message))
-    # print(dir(message))
     print('msg id ', message.message_id)
-    # print('from user ', message.from_user)
-    # print('text ', message.text)
-    # print('views ', message.views)
     if message['media']:
         capt = message['caption']
         caption_entities = message['caption_entities']
-        capt = capt.replace(channels[message["sender_chat"]["username"]],'@SpinorMag')
-        if message['photo']!=None:
-            fid = message['photo']["file_id"]
-            app.send_cached_media('spinor_mag', file_id=fid, caption=capt,caption_entities=caption_entities)
-            
-        if message['video']!=None:
-            fid = message['video']["file_id"]
-            app.send_cached_media('spinor_mag', file_id=fid, caption=capt,caption_entities=caption_entities)
+        if '@'+message["sender_chat"]["username"] in capt:
+            capt = capt.replace(channels[message["sender_chat"]["username"]],'@SpinorMag')
+            if message['photo']!=None:
+                fid = message['photo']["file_id"]
+                if not fid in file_ids:
+                    file_ids.append(fid)
+                    app.send_cached_media('spinor_mag', file_id=fid, caption=capt,caption_entities=caption_entities)
+                
+            if message['video']!=None:
+                fid = message['video']["file_id"]
+                if not fid in file_ids:
+                    file_ids.append(fid)
+                    app.send_cached_media('spinor_mag', file_id=fid, caption=capt,caption_entities=caption_entities)
 
-        
-
+    
 
 my_handler = MessageHandler(my_function, filters.chat(list(channels.keys())))
 app.add_handler(my_handler)
